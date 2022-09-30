31e24becd807a965e6750a6a76932251581b6ac9
AlifSakib <alifsakib@gmail.com>
2022-09-30 15:05:49 +0600

    header added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App font-quickSand">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
